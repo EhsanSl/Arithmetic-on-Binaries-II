a70db1afc3535c3e5251364d00d1557d171a0e8f
Ehsan <76597689+EhsanSl@users.noreply.github.com>
2022-02-08 13:28:08 -0500

    Add files via upload

---
+++ Signed-2's-Complement/complement.h
@@ -0,0 +1,13 @@
+
+#ifndef DD_LAB4_COMPLEMENT_H
+#define DD_LAB4_COMPLEMENT_H
+
+
+void func_1s_comp(int a[], int result[]);
+
+void func_2s_comp(int a[], int result[]);
+
+void func_2s_comp_star(int a[], int result[]);
+
+#endif //DD_LAB4_COMPLEMENT_H
+
